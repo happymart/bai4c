f74a1cb0514945bf5e63d54c1c3064df1a096a68
Hữu Minh <huuminh2101@gmail.com>
2019-02-19 21:21:58 +0700

    bai4c
    so sánh 2 số nguyên

---
+++ main.c
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.c
+ * Author: Administrator
+ *
+ * Created on February 19, 2019, 8:40 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    int a , b ;
+    printf("  Welcome \n");
+   
+    printf("\n Nhập 2 số : \n ");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+        printf("số %d lớn hơn số %d \n",a,b);
+    else
+        if(a<b)
+            printf("số %d nhỏ hơn %d \n",a,b);
+        else{
+            printf("số a bằng số b \n");
+        }
+   
+    return (EXIT_SUCCESS);
+}
+     getch();
